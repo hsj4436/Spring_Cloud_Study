2ea3339ebff4d1265dd2ed4afba5f7ab648607d8
황세진 <hwangsejin@hwangsejin-ui-MacBookPro.local>
2024-02-12 22:48:13 +0900

    섹션 2. API Gateway 내용 추가

--- first-service/src/main/java/org/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/org/example/firstservice/FirstServiceController.java
@@ -1,15 +1,27 @@
 package org.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RequestMapping("/first-service")
 @RestController
 public class FirstServiceController {
+
+    // application.properties(yml)에 등록된 설정 정보를 가져오는 방법 중 하나
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,7 +35,8 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server port = {}", request.getServerPort());
+        return String.format("Hi, there. This is a message from First service on Port %s.", env.getProperty("local.server.port"));
     }
 }
