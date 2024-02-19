3c2c02c3a7ee11a5a87d329b70ca26507d0f9d0f
황세진 <hwangsejin@hwangsejin-ui-MacBookPro.local>
2024-02-20 02:49:01 +0900

    섹션 4 내용 추가
    H2, JPA, ModelMapper, validation, Spring Security 사용을 위한 의존성 추가
    H2 데이터베이스 연동을 위한 설정 추가
    회원 가입 메소드 추가
    회원 가입 시 비밀번호 암호화를 위한 빈 추가
    회원 가입 url을 허용하도록 security 설정 추가

---
+++ user-service/src/main/java/org/example/userservice/controller/UserController.java
@@ -0,0 +1,57 @@
+package org.example.userservice.controller;
+
+import org.example.userservice.dto.UserDto;
+import org.example.userservice.service.UserService;
+import org.example.userservice.vo.Greeting;
+import org.example.userservice.vo.RequestUser;
+import org.example.userservice.vo.ResponseUser;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    private Environment env;
+
+    @Autowired
+    private Greeting greeting;
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(Environment env, UserService userService) {
+        this.env = env;
+        this.userService = userService;
+    }
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+//        return env.getProperty("greeting.message");
+        return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser user) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = mapper.map(user, UserDto.class);
+
+        userService.createUser(userDto);
+
+        ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
+    }
+}
